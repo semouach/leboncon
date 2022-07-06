6198ccffb7a511a173731455540f5aa78c17c025
Slide <82210607+semouach@users.noreply.github.com>
2022-07-06 21:17:40 +0200

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+   production: Boolean,
+   firebase: {
+    apiKey: "AIzaSyBEsYHAgcYeDFxQ-bFsuUEYIK-T4MdvBNI",
+    authDomain: "car-sell-1c477.firebaseapp.com",
+    databaseURL: "https://car-sell-1c477-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "car-sell-1c477",
+    storageBucket: "car-sell-1c477.appspot.com",
+    messagingSenderId: "329058234754",
+    appId: "1:329058234754:web:851ea3fd1e0fb42e6e5f9b"
+   }
+};
